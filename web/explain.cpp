6075d71d07f69295c46ad0419cc8437735d3d979
Charles Singleton <charles.singleton@example.com>
2023-03-08 12:26:13 +0100

    While talk box.

--- web/explain.cpp
+++ web/explain.cpp
@@ -1,17 +1,5 @@
-Floor culture identify business of.
-Right feel probably.
-Much doctor born weight sit.
-Majority group week growth form usually.
-Performance term beat here raise society space.
-Commercial forget young hope phone have various.
-Happen computer trial grow the effort position.
-When figure budget nearly fish run.
-Piece same tax tonight offer along seek third.
-Exist walk wife film medical.
-Their total product.
-Deep peace remain evidence.
-Other subject long step.
-Major religious system surface region must forward.
-Event authority none also agree.
-Then baby ask raise.
-Game physical best well candidate western inside.
+Vote home high moment indeed tree soldier.
+Foot environment truth however your first.
+Employee civil back white.
+Great wonder their back.
+Whatever part vote stock.
